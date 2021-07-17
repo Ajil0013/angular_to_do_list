05385d635d7edd3eb3085d5d210b5779b86db411
Ajil0013 <pajil393@gmail.com>
2021-07-17 19:39:07 +0530

    Change css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list';
-  items:string[]=[];
+  items:{content:string,done:boolean}[]=[];
   task:string='';
 
   addTask(){
     if (this.task==""){}
     else{
-      this.items.push(this.task);
+      this.items.push({content:this.task,done:false});
       this.task="";
     }
   }
 
   deleteTask(index:number){
     this.items.splice(index,1)
+  }
+
+  onDone(index:number){
+    this.items[index].done=true;
   }
 }
